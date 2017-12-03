4ae335815efe51ab26fb0beab7f8ed5a553173a4
lch <1251680944@qq.com>
2017-12-03 18:38:22 +0800

    v 1.0.0.5 *优化文本处理 *完成tf-idf算法

--- src_02/main.py
+++ src_02/main.py
@@ -14,15 +14,19 @@ from nltk_bayes_classifier import *
 def main():
     post_list = []
     vocab_set = set([])
-    dirs = ['env']  # , 'eco', 'pol']
+    a = 0
+    dirs = ['env', 'eco', 'pol']
     for name in dirs:
         for i in range(1, 11):
             res_word_list, doc_set = text_parse(
-                open(u'C:\\Users\\Lich\\Desktop\\test\\' + name + u'\\%d.txt' % i).read().decode('utf-8'))
+                open(u'/Users/lch/Desktop/pycharm/Bayes/test/' + name + u'/%d.txt' % i).read().decode('utf-8'))
             post_list.append(res_word_list)
+            a += len(doc_set)
             vocab_set = vocab_set | doc_set
-    print len(list(vocab_set))
-    get_doc_features(post_list, list(vocab_set))
+    print '文本去除停用词、词形还原后还剩余', len(list(vocab_set)), '个不重复单词。', '\n文本去除停用词后所有文本共含有', a, '个单词'
+    doc_features = get_doc_features(post_list, vocab_set)
+    for i in range(0, len(doc_features)):
+        print doc_features[i], '\n'
 
 
 if __name__ == '__main__':
--- src_02/nltk_bayes_classifier.py
+++ src_02/nltk_bayes_classifier.py
@@ -16,6 +16,7 @@ from nltk.tokenize import *
 from numpy import *
 
 import sys
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 
@@ -28,6 +29,7 @@ __author__ = 'Lich'
 
 def text_parse(input_text):
     sentence = input_text.lower()
+    lemmatizer = WordNetLemmatizer()  # 词形还原
     vocab_set = set([])  # 记录所有出现的单词
     special_tag = ['.', ',', '!', '#', '(', ')', '*', '`', ':', '?', '"', '‘', '’']
     pattern = r""" (?x)(?:[A-Z]\.)+ 
@@ -47,8 +49,9 @@ def text_parse(input_text):
                 or word_tag[i][1] == 'PRP$' or word_tag[i][1] == 'CD':
             pass
         else:
-            res_word_list.append(word_tag[i][0])
-            vocab_set.add(word_tag[i][0])
+            word = lemmatizer.lemmatize(word_tag[i][0])
+            res_word_list.append(word)
+            vocab_set.add(word)
     return res_word_list, vocab_set
 
 
@@ -60,15 +63,40 @@ def text_parse(input_text):
 
 def get_doc_features(input_matrix_data, vocab_set):
     input_matrix = input_matrix_data
-    words_count_matrix = get_lemmatizer(input_matrix)
-    n_contain_dict = calculate_d(input_matrix, vocab_set)
     doc_nums = len(input_matrix)  # 输入的文档总数
-    words_tf = calculate_tf(words_count_matrix)
-    words_idf = calculate_idf(doc_nums, n_contain_dict)
+    words_tf, words_count_matrix = calculate_tf(input_matrix)  # 计算词频
+    n_contain_dict = calculate_d(words_count_matrix, vocab_set)  # 计算包含某单词a 的文档数目
+    words_idf = calculate_idf(doc_nums, n_contain_dict)  # 计算逆文档
+    words_tf_idf, sorted_tf_idf = calculate_tf_idf(doc_nums, words_tf, words_idf)  # 计算一篇文档中单词的tf-idf值
 
-    print  words_tf
-    print('\n\n')
-    print words_idf
+    # 取特征：设置阈值,取tf-idf值大于0.007,这个阈值需要根据分类结果进行调整
+    doc_features = []
+    for i in range(0, len(sorted_tf_idf)):
+        tmp = []
+        for tuple_w in sorted_tf_idf[i]:
+            if tuple_w[1] >= 0.007:
+                tmp.append(tuple_w[0])
+        doc_features.append(tmp)
+        del tmp
+    return doc_features
+
+
+'''
+计算TF-IDF
+'''
+
+
+def calculate_tf_idf(doc_nums, words_tf, words_idf):
+    res = []
+    sorted_res = []
+    for i in range(0, doc_nums):
+        tf_idf_dict = {}
+        for word in words_tf[i]:
+            tf_idf_dict[word] = (words_tf[i][word] * words_idf[word])
+        res.append(tf_idf_dict)
+        sorted_res.append(sorted(tf_idf_dict.items(), lambda x, y: cmp(x[1], y[1]), reverse=True))
+        del tf_idf_dict
+    return res, sorted_res
 
 
 '''
@@ -89,15 +117,19 @@ def calculate_d(words_count_matrix, vocab_set):
 '''
 
 
-def calculate_tf(words_count_matrix):
+def calculate_tf(input_matrix):
     res_list = []
-    for lst in words_count_matrix:
+    words_count = []
+    for lst in input_matrix:
+        words_count.append(Counter(lst))
+
+    for lst in words_count:
         tf_dict = {}
         for word in lst.keys():
             tf_dict[word] = lst[word] / float(sum(lst.values()))  # 计算词频
         res_list.append(tf_dict)
         del tf_dict
-    return res_list
+    return res_list, words_count
 
 
 '''
@@ -108,47 +140,8 @@ def calculate_tf(words_count_matrix):
 def calculate_idf(doc_nums, n_contain_dict):
     idf_dict = {}
     for word in n_contain_dict.keys():
-        idf_dict[word] = log(doc_nums / n_contain_dict[word])
+        idf_dict[word] = log(doc_nums / (n_contain_dict[word]))
     return idf_dict
-
-
-''''
-词形还原
-'''
-
-
-def get_lemmatizer(input_matrix):
-    words_count = []
-    lemmated = []
-    lemmatizer = WordNetLemmatizer()  # 词形还原
-    for lst in input_matrix:
-        for item in lst:
-            lemmated.append(lemmatizer.lemmatize(item))  # 词形还原
-        words_count.append(Counter(lemmated))  # 计算每个词在其文本中出现的次数
-        del lemmated[:]
-    return words_count
-
-
-'''
-词干提取
-'''
-
-
-def get_stem(input_matrix):
-    words_count = []
-    stemmed = []
-    stemmer = PorterStemmer()  # 词干提取
-    for lst in input_matrix:
-        for item in lst:
-            stemmed.append(stemmer.stem(item))  # 词干提取
-        words_count.append(Counter(stemmed))  # 计算每个词在其文本中出现的次数
-        del stemmed[:]
-    return words_count
-
-
-'''
-训练分类器
-'''
 
 
 def train_native_bayes_classifier():
